e8c2958c33b5ab7a3df0cb0469787b6614afda50
amit.kumar <amit131.kumar@paytm.com>
2026-07-24 13:35:27 +0530

    Initial project setup

---
+++ backend/src/main/java/com/localfix/LocalServiceBookingApplication.java
@@ -0,0 +1,14 @@
+package com.localfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalServiceBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                LocalServiceBookingApplication.class, args
+        );
+    }
+}
